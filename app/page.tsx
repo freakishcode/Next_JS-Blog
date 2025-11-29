cbd85b8c01edf3be222776c712c63972bf7b3cd3
Ajibola Bakare (aka Bolaji) <ajibolabakare@gmail.com>
2025-11-29 17:37:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-// Dashboard
+// Dashboard page
 import BlogDashboard from "./dashboard/page";
-
-// import BlogPostForm from "@/components/BlogPostForm";
-
-// import EditPostPage from "@/components/BlogUpdateForm";
 
 export default function Home() {
   return (
     <>
       <BlogDashboard />
-      {/* <BlogPostForm /> */}
-      {/* <EditPostPage /> */}
     </>
   );
 }
